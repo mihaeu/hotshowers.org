d50878068b27205ef8732c9d1d5afa19ab7fca2d
Michael Haeuslmann <haeuslmann@gmail.com>
2016-05-27 10:02:58 +0200

    Add project bootstrap (composer psr4 autoloading, Make, phpunit)

---
+++ src/Sample.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mihaeu\Hotshowers;
+
+class Sample
+{
+    public function __construct()
+    {
+        echo "yes";
+    }
+}
---
+++ tests/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mihaeu\Hotshowers;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertNotNull(new Sample());
+    }
+}
